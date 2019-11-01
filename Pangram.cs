fa8bded06e9f9ad3ebe8987ee7a5ecc475029e88
anudeep1189 <anudeep1189@gmail.com>
2019-11-01 22:05:37 +0530

    Side Track 2 : Pangram

---
+++ Pangram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        int i = 0;
+        char[] Alphabets;
+        Alphabets = new char[]{'a', 'b', 'c', 'd','e','f','g','h','i','j',
+                'k','l','m','n','o','p','q','u','r','s','t','u','v','w','x','y','z'};
+            
+            foreach (char alpha in Alphabets)
+            {
+                foreach (char inputChar in input.ToLower())
+                {
+                    if (alpha == inputChar)
+                    {
+                        i++;
+                        break;
+                    }
+                }
+            }
+            if(i == 27)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+    }
+}
