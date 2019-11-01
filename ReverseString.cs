4ccf36f9e453d8d6099d95a760ffb2fa91a833f2
anudeep1189 <anudeep1189@gmail.com>
2019-11-01 22:04:48 +0530

    Side track 1 : Reverse String

---
+++ ReverseString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        int strSize = input.Length;
+        string strOuput;
+        char[] inputArr = input.ToCharArray();
+        char[] revArr = new char[strSize];
+
+        for(int i = 0, j = strSize - 1;i < strSize;i++,j--)
+        {
+            revArr[i] = inputArr[j];
+        }
+        strOuput = new string(revArr);
+
+        return strOuput;
+
+    }
+}
