9da59971fb00e08f7e271ecd37ab8d2c00b744fb
anudeep1189 <anudeep1189@users.noreply.github.com>
2019-11-06 14:14:39 +0530

    leap : iteration 3

--- Leap.cs
+++ Leap.cs
@@ -2,18 +2,11 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-      //evenly divisible by 100
-      //if it is divisible by 100 it should also be divisible by 400
-      //Except this, all other years evenly divisible by 4 are leap years.
- if ((year % 4 == 0 ) && (year % 100 != 0 || year % 400 == 0))
-      {
-          return true;
-      }
-    else
-    {
-          return false;
-    }
-    }
+  //evenly divisible by 100
+  //if it is divisible by 100 it should also be divisible by 400
+  //Except this, all other years evenly divisible by 4 are leap years.
+  
+  //Using expression-bodied method.
+    public static bool IsLeapYear(int year) => ((year % 4 == 0 ) && (year % 100 != 0 || year % 400 == 0));
+  
 }
