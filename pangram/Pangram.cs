bb0de7bddd27f368d6ac5ec3237fd82358c2049d
anudeep <13384863+anudeep1189@users.noreply.github.com>
2020-03-07 13:52:45 +0530

    solution changed

---
+++ pangram/Pangram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        int i = 0;
+
+            string Alphabets = "abcdefghijklmnopqrstuvwxyz";
+            
+            input = input.ToLower();
+            foreach (char alpha in Alphabets)
+            {
+                foreach (char inputChar in input)
+                {
+                   
+                    if (alpha == inputChar)
+                    {
+                        i++;
+                        break;
+                    } 
+                }
+            }
+            if(i == 26)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+    }
+}
