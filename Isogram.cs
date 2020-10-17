87336c466b06c7d289e228260290fec4f193c738
anudeep1189 <anudeep1189@gmail.com>
2020-10-17 22:22:28 +0530

    isogram
    sol 1

---
+++ Isogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        string strWord = word.ToLower();//all should be equal
+        char[] charWord = strWord.ToCharArray(); 
+        Array.Sort(charWord); // sort to lessen the number of iteration
+        for (int i = 0; i < charWord.Length - 1; i++)
+        {
+            //just need to compare the next one
+            if (charWord[i] == charWord[i + 1] && char.IsLetter(charWord[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
