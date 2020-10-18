f210c88df75f3480f3ba5a03c6bdffab4662c93a
anudeep1189 <anudeep1189@gmail.com>
2020-10-18 11:27:50 +0530

    Update Isogram.cs
    changes 4

--- Isogram.cs
+++ Isogram.cs
@@ -9,13 +9,7 @@ public static class Isogram
         char[] ch = strWord.ToCharArray().Where(c => Char.IsLetter(c)).ToArray(); // get only letter in char array
         var disLetter = ch.Distinct(); // get distinct letter in the char array
 
-        if (disLetter.Count() == ch.Count()) // check if distinct lenght and normal length of the array is same
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return (disLetter.Count() == ch.Count());
+
     }
 }
